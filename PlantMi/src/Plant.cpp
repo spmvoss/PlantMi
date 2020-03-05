0cb345283cef9decf9adc952a8f7a7e052cf29af
mheeniac <sander@rolisa.nl>
2020-03-05 22:09:50 +0100

    Created Plant class

--- PlantMi/src/Plant.cpp
+++ PlantMi/src/Plant.cpp
@@ -10,6 +10,7 @@
 #include "Plant.h"
 #include "Sensors.h"
 #include "Pump.h"
+#include <Arduino.h>
 
 #include <string>
 using namespace std;
@@ -23,6 +24,7 @@ Plant::Plant(string plantName) : needsWatering(_needsWatering), name(_name){
     _hasSoilMoistureSensor = false;
     _hasLightSensor = false;
     _hasPump = false;
+    _hasNoSensors = true;
 }
 
 void Plant::changeName(string name){
@@ -35,6 +37,7 @@ void Plant::addSoilMoistureSensor(int pin, int lowValue, int highValue, int thre
     _soilMoistureSensor.configure(lowValue, highValue);
     _soilMoistureThreshold = threshold;
     _hasSoilMoistureSensor = true;
+    _hasNoSensors = false;
 }
 
 void Plant::addSoilTemperatureSensor(int pin, float T0, float B, float R0){
@@ -42,6 +45,7 @@ void Plant::addSoilTemperatureSensor(int pin, float T0, float B, float R0){
     _soilTemperatureSensor.setPin(pin);
     _soilTemperatureSensor.configure(T0, B, R0);
     _hasSoilTemperatureSensor = true;
+    _hasNoSensors = false;
 }
 
 void Plant::addLightSensor(int pin, float slope, float offset, float dividerR, float voltage){
@@ -49,6 +53,7 @@ void Plant::addLightSensor(int pin, float slope, float offset, float dividerR, f
     _lightSensor.setPin(pin);
     _lightSensor.configure(slope, offset, dividerR, voltage);
     _hasLightSensor = true;
+    _hasNoSensors = false;
 }
 
 void Plant::addPump(int pin, float litrePerHour, int sensorPin = -1, bool sensorLogicReversed = false){
@@ -61,28 +66,46 @@ void Plant::addPump(int pin, float litrePerHour, int sensorPin = -1, bool sensor
     _hasPump = true;
 }
 
+bool Plant::setMoistureLimit(int threshold){
+    _soilMoistureThreshold = threshold;
+    return true;
+}
+
 void Plant::sample(){
-    cout << "Taking measurement sample for " << _name << endl << endl;
+    if(_hasNoSensors){
+        Serial.println("Plant has no sensors, doing nothing");
+        return;
+    }
+    Serial.print("Taking measurement sample for: ");
+    Serial.println(_name);
     if(_hasSoilMoistureSensor){
         _soilMoistureContent = _soilMoistureSensor.measure();
         _needsWatering = (_soilMoistureContent < _soilMoistureThreshold);
-        cout << "Soil Moisture Content: " << _soilMoistureContent << "%" << endl;
+        Serial.print("Soil Moisture Content: ");
+        Serial.print(_soilMoistureContent);
+        Serial.println("%");
     }
     if(_hasSoilTemperatureSensor){
         _soilTemperature = _soilTemperatureSensor.measure();
-        cout << "Soil Temperature: " << _soilTemperature << " Celsius" << endl; 
+        Serial.print("Soil Temperature: ");
+        Serial.print(_soilTemperature);
+        Serial.println(" Celsius");
     }
     if(_hasLightSensor){
         _LUX = _lightSensor.measure();
-        cout << "Light Intensity: " << _LUX << " LUX" << endl << endl;
+        Serial.print("Light Intensity: ");
+        Serial.print(_LUX);
+        Serial.println(" LUX");
     }
     if(_needsWatering && _hasPump){
         bool succeeded = _pump.water();
         if(!succeeded){
             // Do we trigger some sort of notification system?
             // Or make this function return a true/false based on
-            cout << "I could not water " << _name << ", water level low!" << endl;
-        } else {_needsWatering = false; cout << "Succesfully watered " << _name << endl;}
+            Serial.print("Water level for ");
+            Serial.print(_name);
+            Serial.println(" too low!");
+        } else {_needsWatering = false; Serial.print("Succesfully watered "); Serial.println(_name);}
     } else if (_needsWatering && !_hasPump){
         // Do nothing (let the managing class check if _needsWatering and handle accordingly)
         // Or make this trigger some sort of notification system?
